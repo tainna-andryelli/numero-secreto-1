6ed9e3d204eb88ce8e33d76eb5922c988faa38e5
Tainna <tainnaandryelli@gmail.com>
2023-11-20 11:07:44 -0300

    criando projeto

---
+++ app.js
@@ -0,0 +1,26 @@
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+let chute;
+let tentativas = 1;
+
+alert("Boas vindas ao jogo do número secreto!");
+console.log(numeroSecreto);
+
+while (chute != numeroSecreto) {
+  chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}.`);
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`Número secreto é menor que ${chute}`);
+    } else {
+      alert(`Número secreto é maior que ${chute}`);
+    }
+  }
+  tentativas++;
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(
+  `Você descobriu o número secreto '${numeroSecreto}' com ${tentativas} ${palavraTentativa}!`
+);
